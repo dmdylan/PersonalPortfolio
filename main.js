2a3564eb65596c7922ea52a543f2472699f2e9c6
dmdylan <dylandanowski@gmail.com>
2023-11-20 14:58:15 -0500

    Contact me popup
    Added and edited contact me popup and changed about me photo.

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ function scrollToSection(section){
     const element = document.getElementById(`${section}`);
     element.scrollIntoView(true);
 }
+
+function onSubmitContactForm(token) {
+    document.getElementById("contact-form").submit();
+}
+
+function toggleContactForm(){
+    const contactFormElement = document.getElementById("contact-overlay");
+    
+    if(contactFormElement.style.display !== "none"){
+        contactFormElement.style.display = "none";
+    }
+    else{
+        contactFormElement.style.display = "";
+
+    }
+}
+
+function hideContactForm(){
+    document.getElementById("contact-overlay").style.display = "none";
+}
