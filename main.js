6d1b82c92f5d932bb8e0f15a3e5b053fc1421c53
dmdylan <dylandanowski@gmail.com>
2023-11-29 13:09:52 -0500

    Recaptcha verification

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 function scrollToSection(section){
     const element = document.getElementById(`${section}`);
     element.scrollIntoView(true);
@@ -11,16 +13,38 @@ function toggleContactForm(){
     const contactFormElement = document.getElementById("contact-overlay");
     const contactForm = document.getElementById("contact-form");
     
+
     if(contactFormElement.style.display !== "none"){
         contactFormElement.style.display = "none";
         contactForm.reset();
+        grecaptcha.reset();
     }
     else{
         contactFormElement.style.display = "";
     }
 }
 
-// function submitContactForm(submitEvent){
-//     window.alert("Submit button pressed, event listener works");
-//     submitEvent.preventDefault();
-// }
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const captchaResponse = grecaptcha.getResponse();
+
+    if(!captchaResponse.length > 0){
+        throw new Error("captcha not completed");
+    }
+
+    const fd = new FormData(e.target);
+    const params = new URLSearchParams(fd);
+
+    fetch("http://httpbin.org/post",{
+        method: "POST",
+        body: params,
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+    toggleContactForm();
+});
